5548c0c802346dceb0b4426b18e03d7629395d78
Antoine A. Ruzette <antoine.ruzette@gmail.com>
2025-06-14 15:23:06 +0530

    day2: image processing

---
+++ test_matplotlib.py
@@ -0,0 +1,11 @@
+import matplotlib
+matplotlib.use('notebook')  # Use notebook backend
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create a simple plot
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.plot(x, y)
+plt.title('Test Plot')
+plt.show() 
